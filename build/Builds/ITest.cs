b2294e5bea43035ea3885e14fc31e5279af6a0a1
Tomo Masakura <tomo.masakura@gmail.com>
2022-01-04 13:59:06 +0900

    add test nuke target

---
+++ build/Builds/ITest.cs
@@ -0,0 +1,19 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
+
+namespace Builds;
+
+interface ITest : ICompile
+{
+    // ReSharper disable once UnusedMember.Global
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore());
+        });
+}
