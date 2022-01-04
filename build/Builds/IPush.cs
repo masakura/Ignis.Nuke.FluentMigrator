b9a42cce30d813fc022e4c6549ee884cd8aca78d
Tomo Masakura <tomo.masakura@gmail.com>
2022-01-04 18:03:30 +0900

    add nuke push target

---
+++ build/Builds/IPush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.ValueInjection.ValueInjectionUtility;
+
+namespace Builds;
+
+interface IPush : IPack
+{
+    private const string NuGetApiKeyEnvironment = "NUGET_API_KEY";
+
+    [Parameter($"NuGet API Key. Default, read from the NUGET_API_KEY environment variable.{NuGetApiKeyEnvironment}")]
+    string NugetApiKey => TryGetValue(() => NugetApiKey) ??
+                          Environment.GetEnvironmentVariable(NuGetApiKeyEnvironment);
+
+    [Parameter("Push NuGet source. Default is `nuget.org`.")]
+    string NugetSource => TryGetValue(() => NugetSource) ?? "nuget.org";
+
+    // ReSharper disable once UnusedMember.Global
+    Target Push => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            Logger.Info(NugetApiKey);
+            DotNetTasks.DotNetNuGetPush(s => s
+                .SetTargetPath(NuPkgDirectory / "*.nupkg")
+                .SetApiKey(NugetApiKey)
+                .SetSource(NugetSource));
+        });
+
+}
