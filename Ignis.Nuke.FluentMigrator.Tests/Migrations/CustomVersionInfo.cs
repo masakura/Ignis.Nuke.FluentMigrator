550344a0f36c230ec5167438a5470d12b0a6c8fc
Tomo Masakura <tomo.masakura@gmail.com>
2022-01-05 21:10:51 +0900

    auto detect metadata inside assembly

---
+++ Ignis.Nuke.FluentMigrator.Tests/Migrations/CustomVersionInfo.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator.Runner.VersionTableInfo;
+
+namespace Ignis.Nuke.FluentMigrator.Migrations;
+
+[VersionTableMetaData]
+// ReSharper disable once UnusedType.Global
+public sealed class CustomVersionInfo : IVersionTableMetaData
+{
+#pragma warning disable CS0618
+    private readonly IVersionTableMetaData _default = new DefaultVersionTableMetaData();
+#pragma warning restore CS0618
+    
+    public object? ApplicationContext { get; set; } = null;
+
+    public bool OwnsSchema => _default.OwnsSchema;
+    public string SchemaName => _default.SchemaName;
+    public string TableName => $"Custom{_default.TableName}";
+    public string ColumnName => _default.ColumnName;
+    public string DescriptionColumnName => _default.DescriptionColumnName;
+    public string UniqueIndexName => _default.UniqueIndexName;
+    public string AppliedOnColumnName => _default.AppliedOnColumnName;
+}
--- Ignis.Nuke.FluentMigrator/FluentMigratorMigrateUpSettings.cs
+++ Ignis.Nuke.FluentMigrator/FluentMigratorMigrateUpSettings.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using FluentMigrator.Runner;
+﻿using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ignis.Nuke.FluentMigrator;
@@ -7,13 +7,14 @@ namespace Ignis.Nuke.FluentMigrator;
 public sealed class FluentMigratorMigrateUpSettings
 {
     public List<Action<IMigrationRunnerBuilder>> ConfigureRunners { get; } = new();
-    public List<Assembly> Assemblies { get; } = new();
+    public List<string> Assemblies { get; } = new();
 
     internal IServiceCollection ConfigureServices()
     {
         var services = new ServiceCollection()
             .AddFluentMigratorCore()
-            .ConfigureRunner(rb => rb.ScanIn(Assemblies.ToArray()).For.Migrations());
+            .ConfigureRunner(rb =>
+                rb.Services.Configure<AssemblySourceOptions>(options => options.AssemblyNames = Assemblies.ToArray()));
 
         foreach (var configure in ConfigureRunners) services.ConfigureRunner(configure);
         return services;
